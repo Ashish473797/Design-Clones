7f0d2418a01650a7546aeb0a4eaac4c70aad6189
Ashish473797 <ashish473797@gmail.com>
2023-11-26 19:01:25 +0530

    updated

---
+++ 05-Slack-Design/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        "primary": "#541554",
+        "secondary": "#4a154b",
+        "light-gray": "#F4EDE4",
+        "dark-yellow": "#ECB22E",
+        "regular-blue": "#1264A3",
+        "black": "#000000",
+        "white": "#FFFFFF",
+        "gray": "#555555",
+        "blue": "#4285f4",
+      },
+    },
+  },
+  plugins: [],
+}
+
