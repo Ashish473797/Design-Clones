4fc15fc392779858372a76f043f45b365f16b484
Ashish473797 <ashish473797@gmail.com>
2023-12-03 18:42:38 +0530

    updated

--- 06-Gmail-Design/tailwind.config.js
+++ 06-Gmail-Design/tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
         poppins: ['Poppins', 'sans-serif'],
       },
       colors: {
-        
+        "primary": "#F6F8FC",
+        "secondary": "#EAEBEF",
+        "input": "#EAF1FB",
+        "compose": "#C2E7FF",
+        "inbox": "#D3E3FD",
+        "gray": "#5F6368",
+        "secondary-gray": "#5F6368",
+        "light-gray": "#676A69",
+        "primary-blue": "#4C82D8",
       }, 
     },
   },
