9bdcbab49a2aeee9de937a2774e80506968f32c8
Ashish473797 <ashish473797@gmail.com>
2023-12-11 16:31:01 +0530

    updated

---
+++ 07-Youtube-Design/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
