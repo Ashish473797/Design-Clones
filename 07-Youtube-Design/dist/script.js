546555b48832eb0782971c2c0046c13422f710e9
Ashish473797 <ashish473797@gmail.com>
2023-12-16 07:22:38 +0530

    updated

--- 07-Youtube-Design/dist/script.js
+++ 07-Youtube-Design/dist/script.js
@@ -4,4 +4,10 @@ const body = document.querySelector("body");
 
 themeToggler.addEventListener("click", ()=>{
     body.classList.toggle("dark");
+    if(themeToggler.innerText == "Light Mode"){
+        themeToggler.innerText = "Dark Mode"
+    }
+    else{
+        themeToggler.innerText = "Light Mode";
+    }
 })
